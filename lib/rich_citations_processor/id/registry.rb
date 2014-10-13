09da6936f3b5d06beacffce23a68d6ef52239bbe
Marc Rohloff <marc@marcrohloff.com>
2014-10-13 16:05:44 -0600

    Started a centralized system for identifying and parsing identifiers of different types (This is a work in progress)

---
+++ lib/rich_citations_processor/id/registry.rb
@@ -0,0 +1,62 @@
+# Copyright (c) 2014 Public Library of Science
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+module RichCitationsProcessor
+  module ID
+
+    class Registry
+
+      class << self
+
+        def add(id_class)
+          @@id_classes << id_class
+        end
+
+        def lookup(type, identifier)
+          load unless @@classes_loaded
+          @@id_classes.find do |klass| klass.matches?(type, identifier) end
+        end
+
+        def lookup!(type, identifier)
+          lookup(type, identifier) || raise("Unable to locate ID type for #{type.inspect}:#{identifier.inspect}")
+        end
+
+        # Load all the files in this directory to populate the Registry
+        def load
+          path = File.join( File.dirname(__FILE__), '*.rb')
+          Dir[path].each do |file|
+            ActiveSupport::Dependencies.require_or_load(file)
+          end
+          @@classes_loaded = true
+        end
+
+        private
+
+        @@id_classes = []
+        @@classes_loaded = false
+
+      end
+
+    end
+  end
+end
+
+
+
